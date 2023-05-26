b876842beb1145f012a88e9f930f697ab26036b8
Skjuber <ante.22@hotmail.com>
2023-05-26 11:51:13 +0200

    Skeleton for a project created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+interface Article {
+  title: string;
+  category: string;
+  publishedAt: Date;
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    articles: [
+      {
+        title: "Article 1",
+        category: "Technology",
+        publishedAt: new Date(),
+      },
+      {
+        title: "Article 2",
+        category: "Business",
+        publishedAt: new Date(),
+      },
+      {
+        title: "Article 3",
+        category: "Sports",
+        publishedAt: new Date(),
+      },
+    ],
+    favorites: [],
+  };
+
+  handleClick = (article: Article) => {
+    this.setState({
+      favorites: [...this.state.favorites, article],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Newsy</h1>
+        <div>
+          <ul>
+            {this.state.articles.map((article, index) => (
+              <li key={index}>
+                <h2>{article.title}</h2>
+                <p>{article.category}</p>
+                <p>{article.publishedAt.toString()}</p>
+                <button onClick={() => this.handleClick(article)}>
+                  Bookmark
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
