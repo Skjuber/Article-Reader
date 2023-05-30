cb9f209782258c7888a814004b8fea0e64585a71
Skjuber <ante.22@hotmail.com>
2023-05-30 20:11:51 +0200

    Flexbox adjussted for bookmark icon in categories page

--- src/app/components/UI/Category.tsx
+++ src/app/components/UI/Category.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { Article } from "../../App";
-
 import ArticleActions from "../store/reducers/ArticleActions";
 import "./Category.scss";
+
 interface CategoryProps {
   articlesByCategory: { [key: string]: Article[] };
 }
@@ -11,14 +11,12 @@ interface CategoryProps {
 const Category: React.FC<CategoryProps> = ({ articlesByCategory }) => {
   const { category } = useParams<{ category?: string }>();
 
-  // Add a check for undefined category
   if (!category) {
     return <p>No category selected.</p>;
   }
 
   const articles = articlesByCategory[category];
 
-  // Check for undefined articles
   if (!articles) {
     return <p>No articles found for category {category}.</p>;
   }
@@ -26,14 +24,18 @@ const Category: React.FC<CategoryProps> = ({ articlesByCategory }) => {
   return (
     <div>
       <h1 className="categories-heading">{category}</h1>
-
       <ul>
         {articles.map((article: Article, index: number) => (
           <li key={index}>
-            <h3>{article.title}</h3>
+            <div className="article-header categories">
+              <div className="title categories">
+                <h3>{article.title}</h3>
+                <div className="bookmark-button categories">
+                  <ArticleActions article={article} />
+                </div>
+              </div>
+            </div>
             <p>{article.publishedAt.toString()}</p>
-
-            <ArticleActions article={article} />
           </li>
         ))}
       </ul>
