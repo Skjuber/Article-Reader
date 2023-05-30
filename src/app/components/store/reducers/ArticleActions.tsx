ce25ae9b35bb0f96111286e69a14ffd7e7513b9a
Skjuber <ante.22@hotmail.com>
2023-05-30 18:38:17 +0200

    ArticleActions.tsx updated

--- src/app/components/store/reducers/ArticleActions.tsx
+++ src/app/components/store/reducers/ArticleActions.tsx
@@ -3,34 +3,38 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store";
 import { addArticle, removeArticle } from "./FavoriteArticlesSlice";
 import { Article } from "../../../App";
+import { FiBookmark, FiMinus } from "react-icons/fi";
 
 interface ArticleActionsProps {
   article: Article;
 }
 
 const ArticleActions: React.FC<ArticleActionsProps> = ({ article }) => {
+  const favorites = useSelector(
+    (state: RootState) => state.favoriteArticles.value
+  );
   const dispatch = useDispatch();
 
-  const isArticleFavorite = useSelector((state: RootState) =>
-    state.favoriteArticles.value.some(
-      (favorite) => favorite.title === article.title
-    )
-  );
-
-  const toggleFavorite = () => {
-    if (isArticleFavorite) {
-      dispatch(removeArticle(article));
-    } else {
+  const handleClick = () => {
+    if (!favorites.some((favorite) => favorite.title === article.title)) {
       dispatch(addArticle(article));
+    } else {
+      dispatch(removeArticle(article));
     }
   };
 
+  const isFavorite = favorites.some(
+    (favorite) => favorite.title === article.title
+  );
+
   return (
-    <div>
-      <button onClick={toggleFavorite}>
-        {isArticleFavorite ? "Remove from Favorites" : "Add to Favorites"}
-      </button>
-    </div>
+    <button
+      onClick={handleClick}
+      title={isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+      className="article-action"
+    >
+      {isFavorite ? <FiMinus /> : <FiBookmark />}
+    </button>
   );
 };
 
