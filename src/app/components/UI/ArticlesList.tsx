5a6cd502167aebdbda8b0ee749e12940167495d1
Skjuber <ante.22@hotmail.com>
2023-05-30 21:45:31 +0200

    AriclesList.tsx added, to be imported yet

---
+++ src/app/components/UI/ArticlesList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Article } from "../../App";
+import ArticleActions from "../store/reducers/ArticleActions";
+
+interface ArticlesListProps {
+  articles: Article[];
+}
+
+const ArticlesList: React.FC<ArticlesListProps> = ({ articles }) => {
+  return (
+    <ul className="articles-list">
+      {articles.map((article: Article, index: number) => (
+        <li key={index}>
+          <div className="article-header">
+            <h3>{article.title}</h3>
+            <ArticleActions article={article} />
+          </div>
+          <p>{article.publishedAt.toString()}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ArticlesList;
