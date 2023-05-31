b93282e0c82883fad791cc28cf5bd3f5333fafa5
Skjuber <ante.22@hotmail.com>
2023-05-31 16:14:43 +0200

    Title in Header section color and font size changed

--- src/app/components/UI/Header.tsx
+++ src/app/components/UI/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./Search";
+import "./Header.scss";
 
 interface HeaderProps {
   onSearch: (query: string) => void;
@@ -7,7 +8,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ onSearch }) => {
   return (
-    <div>
+    <div className="header">
       <h1>Article-reader</h1>
       <Search onSearch={onSearch} />
     </div>
