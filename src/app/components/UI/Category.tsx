5aeb517a0f3c9a21c31d04e1dd3cea90b19798ab
Skjuber <ante.22@hotmail.com>
2023-05-31 12:09:44 +0200

    ArticlesList logic applied to Category.tsx component

--- src/app/components/UI/Category.tsx
+++ src/app/components/UI/Category.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { Article } from "../../App";
-import ArticleActions from "../store/reducers/ArticleActions";
+import ArticlesList from "./ArticlesList";
 import "./Category.scss";
 
 interface CategoryProps {
@@ -24,22 +24,7 @@ const Category: React.FC<CategoryProps> = ({ articlesByCategory }) => {
   return (
     <div>
       <h1 className="categories-heading">{category}</h1>
-      <ul>
-        {articles.map((article: Article, index: number) => (
-          <li key={index}>
-            <div className="article-header categories">
-              <div className="title categories">
-                <h3>{article.title}</h3>
-
-                <div className="bookmark-button categories">
-                  <ArticleActions article={article} />
-                </div>
-              </div>
-            </div>
-            <p>{article.publishedAt.toString()}</p>
-          </li>
-        ))}
-      </ul>
+      <ArticlesList articles={articles} />
       <Link to="/">Return to Home</Link>
     </div>
   );
